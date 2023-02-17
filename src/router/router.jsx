efca10d21fba13ec815cdf1c4ffc5db942aa2a6b
Vinicios Neves <vinny@macbook.lan>
2023-02-17 19:41:45 +0000

    aula 2 - video 1.1

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,7 +4,25 @@ import PaginaInicial from "../paginas/PaginaInicial"
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        children: [
+            {
+                path:'cadastro',
+                element: <h1>Selecão de tipo de cliente</h1>,
+                children: [
+                    {
+                        path: 'cliente',
+                        element: <h1>Interesesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluído</h1>
+                    }
+                ]
+            }
+        ],
     },
 ]);
