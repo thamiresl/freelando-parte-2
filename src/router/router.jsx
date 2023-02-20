56efebc6c8e94a599d0651e4247950b3cb694549
Vinicios Neves <vinny@macbook.lan>
2023-02-20 20:00:45 +0000

    aula 3 - video 3.3

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom"
+import DadosPessoais from "../paginas/cadastro/DadosPessoais";
+import Interesses from "../paginas/cadastro/Interesses";
 import LayoutBaseCadastro from "../paginas/cadastro/LayoutBaseCadastro";
 import SelecaoCliente from "../paginas/cadastro/SelecaoCliente";
 import LayoutBase from "../paginas/LayoutBase";
-import PaginaInicial from "../paginas/PaginaInicial"
 
 export const router = createBrowserRouter([
     {
@@ -19,11 +20,11 @@ export const router = createBrowserRouter([
                     },
                     {
                         path: 'interesses',
-                        element: <h1>Interesesses</h1>
+                        element: <Interesses />
                     },
                     {
                         path: 'dados-pessoais',
-                        element: <h1>Dados pessoais</h1>
+                        element: <DadosPessoais />
                     },
                     {
                         path: 'concluido',
