b0c59e602503114182e21e3a276c3bfee8c8686b
Vinicios Neves <vinny@macbook.lan>
2023-02-20 20:36:45 +0000

    aula 4 - video 4.3

--- src/contexto/CadastroUsuario.jsx
+++ src/contexto/CadastroUsuario.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const usuarioInicial = {
     perfil: '',
@@ -22,3 +22,93 @@ export const CadastroUsuarioContext = createContext({
     setSenha: () => null,
     setSenhaConfirmada: () => null,
 })
+
+export const useCadastroUsuarioContext = () => {
+    return useState(CadastroUsuarioContext);
+}
+
+export const CadastroUsuarioProvider = ({ children }) => {
+
+    const [usuario, setUsuario] = useState(usuarioInicial)
+
+    const setPerfil = (perfil) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                perfil
+            }
+        })
+    }
+    const setInteresse = (interesse) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                interesse
+            }
+        })
+    }
+    const setNomeCompleto = (nomeCompleto) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                nomeCompleto
+            }
+        })
+    }
+    const setUf = (uf) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                uf
+            }
+        })
+    }
+    const setCidade = (cidade) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                cidade
+            }
+        })
+    }
+    const setEmail = (email) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                email
+            }
+        })
+    }
+    const setSenha = (senha) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                senha
+            }
+        })
+    }
+    const setSenhaConfirmada = (senhaConfirmada) => {
+        setUsuario(estadoAnterior => {
+            return {
+                ...estadoAnterior,
+                senhaConfirmada
+            }
+        })
+    }
+
+    const contexto = {
+        usuario,
+        setPerfil,
+        setInteresse,
+        setNomeCompleto,
+        setUf,
+        setCidade,
+        setEmail,
+        setSenha,
+        setSenhaConfirmada,
+    }
+
+    return (<CadastroUsuarioContext.Provider value={contexto}>
+        {children}
+    </CadastroUsuarioContext.Provider>)
+}
