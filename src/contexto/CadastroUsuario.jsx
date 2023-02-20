b8e650a057f88368f378e8edd276203bad843e6f
Vinicios Neves <vinny@macbook.lan>
2023-02-20 20:27:20 +0000

    aula 4 - video 4.2

---
+++ src/contexto/CadastroUsuario.jsx
@@ -0,0 +1,24 @@
+import { createContext } from 'react';
+
+const usuarioInicial = {
+    perfil: '',
+    interesse: '',
+    nomeCompleto: '',
+    uf: '',
+    cidade: '',
+    email: '',
+    senha: '',
+    senhaConfirmada: ''
+}
+
+export const CadastroUsuarioContext = createContext({
+    usuario: usuarioInicial,
+    setPerfil: () => null,
+    setInteresse: () => null,
+    setNomeCompleto: () => null,
+    setUf: () => null,
+    setCidade: () => null,
+    setEmail: () => null,
+    setSenha: () => null,
+    setSenhaConfirmada: () => null,
+})
